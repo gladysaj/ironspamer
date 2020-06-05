2031badbeed8c1c7c92629166ad17dca9eb07a69
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-06-04 19:37:09 -0500

    Add mongoose and dotenv

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -5,6 +7,17 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((x) => {
+  console.log(`Connected to Mongo! Database name "{x.connections[0].name}"`
+  );
+}).catch((err) => {
+  console.error('Error connecting to mongo', err)
+});
 
 const app = express();
 
